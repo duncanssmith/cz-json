3434908c9c3503891b15ffff2861f00571596fc3
Duncan Smith <duncanssmith@gmail.com>
2022-10-14 02:21:12 +0100

    Styling for css nav bar

--- routes/index.js
+++ routes/index.js
@@ -23,6 +23,7 @@ router.get('/', function(req, res, next) {
   json2: cadenzas, 
   json3: scales, 
   tableLimit: tableLimit,
+  home: true,
   page: 'Mark Knight Cadenzas and Scales', 
   page2: 'Cadenzas', 
   page3: 'Scales'});
@@ -31,45 +32,45 @@ router.get('/', function(req, res, next) {
 router.get('/Cadenzas', function(req, res, next) {
   const products = productsFile 
   const cadenzas = cadenzasFile 
-  res.render('0_List', { title: 'Mark Knight Cadenzas', json: cadenzas, products: products, page: 'Cadenzas' });
+  res.render('0_List', { title: 'Mark Knight Cadenzas', cadenzas:true, json: cadenzas, products: products, page: 'Cadenzas' });
 });
 
 router.get('/Cadenzas/:id', function(req, res, next) {
   const products = productsFile 
   var id = req.params.id
   const item = cadenzasFile[id]
-  res.render('1_Item', { title: 'Mark Knight Cadenzas', item: item, products: products, page: 'Cadenzas', index: id});
+  res.render('1_Item', { title: 'Mark Knight Cadenzas', item: item, cadenzas:true, products: products, page: 'Cadenzas', index: id});
 });
 
 router.get('/Scales', function(req, res, next) {
   const scales = scalesFile 
-  res.render('0_List', { title: 'Mark Knight Scales', json: scales, page: 'Scales' });
+  res.render('0_List', { title: 'Mark Knight Scales', json: scales, scales:true, page: 'Scales' });
 });
 
 router.get('/Scales/:id', function(req, res, next) {
   var id = req.params.id
   const item = scalesFile[id] 
-  res.render('1_Item', { title: 'Mark Knight Scales', item: item, page: 'Scales', index: id});
+  res.render('1_Item', { title: 'Mark Knight Scales', item: item, scales:true, page: 'Scales', index: id});
 });
 
 router.get('/Publications', function(req, res, next) {
   const products = productsFile 
-  res.render('0_List', { title: 'Mark Knight All Publications', json: products, page: 'Publications' });
+  res.render('0_List', { title: 'Mark Knight All Publications', publications: true, json: products, page: 'Publications' });
 });
 
 router.get('/Publications/:id', function(req, res, next) {
   var id = req.params.id
   const item = productsFile[id] 
-  res.render('1_Item', { title: 'Mark Knight All Publications', item: item, page: 'Publications', index: id});
+  res.render('1_Item', { title: 'Mark Knight All Publications', publications: true, item: item, page: 'Publications', index: id});
 });
 
 router.get('/About', function(req, res, next) {
   const about = aboutFile 
-  res.render('3_About', { title: 'Mark Knight About', json: about, page: 'About' });
+  res.render('3_About', { title: 'Mark Knight About', about:true, json: about, page: 'About' });
 });
 
 router.get('/Contact', function(req, res, next) {
-  res.render('4_Contact', { title: 'Mark Knight Contact' });
+  res.render('4_Contact', { title: 'Mark Knight Contact', contact:true, page: 'Contact' });
 });
 
 module.exports = router;
