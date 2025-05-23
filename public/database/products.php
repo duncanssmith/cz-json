8343e5a997d4c73408833fc602d82b4daa4b0e45
Duncan Smith <duncansmith@Mac.communityfibre.co.uk>
2025-05-23 16:57:10 +0100

    Add links to NymetMusic.com

--- public/database/products.php
+++ public/database/products.php
@@ -25,6 +25,28 @@ echo "done\n";
 
 /*
 [
+'all' => 'https://nymetmusic.com/product-category/solo-instruments/mark-knight-cadenzas-and-scales/',
+
+'link' => 'https://nymetmusic.com/product/cadenzas-for-carl-stamitz-viola-concerto-no-1-in-d-op-1/',
+'link' => 'https://nymetmusic.com/product/cadenzas-for-hoffmeister-viola-concerto-in-d-mark-knight/',
+'link' => 'https://nymetmusic.com/product/cadenzas-for-mozart-violin-concertos-no-1-in-b-flat-k207-no-2-in-d-k211-mark-knight/',
+'link' => 'https://nymetmusic.com/product/cadenzas-for-haydn-cello-concerto-in-c-hob-viib1-mark-knight/',
+'link' => 'https://nymetmusic.com/product/concerto-in-c-major-for-viola-haydn-hob-viib1-transcribed-with-candenzas-mark-knight/',
+
+'link' => 'https://nymetmusic.com/product/carl-flesch-plus-scales-for-viola-volume-i-collated-and-fingered-by-mark-knight/',
+'link' => 'https://nymetmusic.com/product/carl-flesch-plus-scales-for-viola-volume-ii-collated-and-fingered-by-mark-knight/',
+'link' => 'https://nymetmusic.com/product/carl-flesch-plus-scales-for-viola-volume-iii-collated-and-fingered-by-mark-knight/',
+'link' => 'https://nymetmusic.com/product/carl-flesch-plus-scales-for-violin-volume-i-collated-and-fingered-by-mark-knight/',
+'link' => 'https://nymetmusic.com/product/carl-flesch-plus-scales-for-violin-volume-ii-collated-and-fingered-by-mark-knight/',
+'link' => 'https://nymetmusic.com/product/carl-flesch-plus-scales-for-violin-volume-iii-collated-and-fingered-by-mark-knight/',
+]
+*/
+
+
+
+/*
+
+[
 'all' => '/shop/powersearch/powersearch_results?search=Strings+Attached',
 
 'link' => 'https://boosey.com/shop/prod/Knight-Mark-Cadenzas-for-Carl-Stamitz-Viola-Concerto-No-1-in-D-op-1/2067549',
@@ -35,7 +57,6 @@ echo "done\n";
 'link' => 'https://boosey.com/shop/prod/Flesch-Carl-Carl-Flesch-Plus-Scales-for-Viola-Volume-I/2067557',
 'link' => 'https://boosey.com/shop/prod/Flesch-Carl-Carl-Flesch-Plus-Scales-for-Viola-Volume-II/2067546',
 'link' => 'https://boosey.com/shop/prod/Knight-Mark-Carl-Flesch-Plus-Scales-For-Viola-Vol-III/2364993',
-
 'link' => 'https://boosey.com/shop/prod/Flesch-Carl-Carl-Flesch-Plus-Scales-for-Violin-Volume-I/2063118',
 'link' => 'https://boosey.com/shop/prod/Flesch-Carl-Carl-Flesch-Plus-Scales-for-Violin-Volume-II/2067554',
 'link' => 'https://boosey.com/shop/prod/Knight-Mark-Carl-Flesch-Plus-Scales-For-Violin-Vol-III/2364995',
